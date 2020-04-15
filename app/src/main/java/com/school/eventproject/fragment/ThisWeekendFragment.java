da3a91932f0b9893054a12fd3a4eb6e293cc11fa
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-04-15 18:32:29 +0630

    - Refactored home fragment's fragment UI.

--- app/src/main/java/com/school/eventproject/fragment/ThisWeekendFragment.java
+++ app/src/main/java/com/school/eventproject/fragment/ThisWeekendFragment.java
@@ -4,10 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.school.eventproject.R;
 
@@ -16,8 +16,7 @@ public class ThisWeekendFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_main, container, false);
-        TextView tv = root.findViewById(R.id.section_label);
-        tv.setText("This is this weekend fragment");
+        RecyclerView rv = root.findViewById(R.id.rv);
         return root;
     }
 }
