e92dc450887fc5a6b09d72ba117eb8f304157047
NaingMyoThawNMYT <naingmyothaw.nmyt@gmail.com>
2020-04-15 07:56:07 +0630

    - Base bottom navigation frame is added.

---
+++ app/src/main/java/com/school/eventproject/fragment/WishListFragment.java
@@ -0,0 +1,23 @@
+package com.school.eventproject.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.school.eventproject.R;
+
+public class WishListFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_wish_list, container, false);
+        final TextView textView = root.findViewById(R.id.text_wish_list);
+        textView.setText("This is wishlist fragment");
+        return root;
+    }
+}
